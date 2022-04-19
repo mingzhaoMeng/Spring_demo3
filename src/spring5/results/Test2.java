130c935998bad391414b47a4ecf5f63b5a18bbda
mengmzh <mingzhao.meng@qq.com>
2022-04-19 15:15:35 +0800

    branch分支第一个版本

--- src/spring5/results/Test2.java
+++ src/spring5/results/Test2.java
@@ -3,5 +3,7 @@ package spring5.results;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("真厉害");
+        System.out.println("branch-commit:提交第一次");
+
     }
 }
