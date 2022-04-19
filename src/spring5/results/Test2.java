c35933db9869d013dbb0582129666ab8a7967cc1
mengmzh <mingzhao.meng@qq.com>
2022-04-19 15:17:19 +0800

    master分支第一个版本

--- src/spring5/results/Test2.java
+++ src/spring5/results/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("真厉害");
         System.out.println("真厉害22");
+        System.out.println("master-commit:第一个版本");
     }
 }
